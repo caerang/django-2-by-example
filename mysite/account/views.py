04081d777e2f30a88db72b8eb0bf5a9bb7c69798
Hyeongdo Lee <mylovercorea@gmail.com>
2018-08-07 15:42:41 +0900

    Add Adding user actions to the activity stream

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 
+from actions.utils import create_action
 from common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm,\
                    UserEditForm, ProfileEditForm
@@ -49,6 +50,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -114,6 +116,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
