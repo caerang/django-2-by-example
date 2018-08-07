c9a7d87c23880b533236013ea3c5744143a282df
Hyeongdo Lee <mylovercorea@gmail.com>
2018-08-07 20:30:38 +0900

    Add Storing item views in Redis and redis settings in docker-compose

--- mysite/conf/settings.py
+++ mysite/conf/settings.py
@@ -182,3 +182,8 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+
+# Redis
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+REDIS_DB = 0
--- mysite/images/views.py
+++ mysite/images/views.py
@@ -1,14 +1,21 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+import redis
 
 from actions.utils import create_action
 from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
+
+# connect to redis
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 @login_required
@@ -36,10 +43,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr('image:{}:views'.format(image.id))
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @ajax_required
