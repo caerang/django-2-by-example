485b0b9b0b9cb95531bb0ce65795602134bb6dfa
Hyeongdo Lee <mylovercorea@gmail.com>
2018-08-15 11:17:12 +0900

    Add Sending PDF files by email

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -1,5 +1,12 @@
+from io import BytesIO
+
 import braintree
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+import weasyprint
+
 from orders.models import Order
 
 
@@ -20,6 +27,24 @@ def payment_process(request):
             order.paid = True
             order.braintree_id = result.transaction.id
             order.save()
+            subject = 'My Shop - Invoice no. {}'.format(order.id)
+            message = 'Please, find attached the invocie for \
+            your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+            html = render_to_string('orders/order/pdf.html',
+                                    {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(
+                settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out,
+                                                   stylesheets=stylesheets)
+            email.attach_file('order_{}.pdf'.format(order.id),
+                              out.getvalue(),
+                              'application/pdf')
+            email.send()
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
@@ -30,8 +55,10 @@ def payment_process(request):
                       {'order': order,
                        'client_token': client_token})
 
+
 def payment_done(request):
     return render(request, 'payment/done.html')
+
 
 def payment_canceled(request):
     return render(request, 'payment/canceled.html')
